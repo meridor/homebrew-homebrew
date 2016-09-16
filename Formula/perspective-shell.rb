c923c99491e17b03af86f2f499371cb5cd595e8e
Ivan Krutov <vania-pooh@yandex-team.ru>
2016-09-16 13:58:40 +0400

    Updated shell to 1.3.1

--- Formula/perspective-shell.rb
+++ Formula/perspective-shell.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class PerspectiveShell < Formula
   homepage 'https://github.com/meridor/perspective-backend'
-  url 'https://github.com/meridor/perspective-backend/releases/download/perspective-backend-1.3.0/perspective-shell.zip'
+  url 'https://github.com/meridor/perspective-backend/releases/download/perspective-backend-1.3.1/perspective-shell.zip'
   head 'https://github.com/meridor/perspective-backend.git'
-  sha256 '9c62f172c6cc4a7354f2b291e5627212019165c6d93d4fae2b523f56330d5543'
-  version '1.3.0'
+  sha256 '6a8d514cf77ea364d95bf76a39c697f55132a649748524548ab00a9bd96c2def'
+  version '1.3.1'
 
 
   def install
