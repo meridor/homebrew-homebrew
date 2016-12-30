7b4b96b4db013d2d2e83eb8aa2630a6029543dbc
Ivan Krutov <vania-pooh@vania-pooh.com>
2016-12-30 06:38:45 +0300

    Added devel section to Homebrew

--- Formula/perspective-shell.rb
+++ Formula/perspective-shell.rb
@@ -7,6 +7,10 @@ class PerspectiveShell < Formula
   sha256 '112f80732c37d0ad0d6ac1fd988f45e4439561fd01264bfbb5d84fc2b85ac7a3'
   version '1.3.3'
 
+  devel do
+    url 'https://github.com/meridor/perspective-backend/releases/download/perspective-backend-1.3.4-RC1/perspective-shell.zip'
+    sha256 "2a2ba417eebaadcb4418ee7b12fe2998f26d6e6f7fda7983412ff66a741ab6f7"
+  end
 
   def install
     jarname = 'perspective-shell.jar'
